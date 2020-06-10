3e59457d5b3cc7523ab082f5b420d2cb7211cf51
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-10 17:21:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,7 +15,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-maru_cat = remove_element_from_end_of_array.pop    
+maru_cat = array.pop    
 end
 
 def remove_element_from_start_of_array(array)
